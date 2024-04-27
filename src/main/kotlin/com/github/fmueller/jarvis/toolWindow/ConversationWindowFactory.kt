59d73434df34ced5a52cb850268e72c365e64071
Felix Müller <felix@fmueller.io>
2024-04-27 12:17:52 +0200

    Rename some generated code and delete not needed code

--- src/main/kotlin/com/github/fmueller/jarvis/toolWindow/ConversationWindowFactory.kt
+++ src/main/kotlin/com/github/fmueller/jarvis/toolWindow/ConversationWindowFactory.kt
@@ -1,33 +1,28 @@
 package com.github.fmueller.jarvis.toolWindow
 
+import com.github.fmueller.jarvis.MyBundle
+import com.github.fmueller.jarvis.services.MyProjectService
+import com.github.fmueller.jarvis.services.OllamaService
 import com.intellij.openapi.components.service
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.fmueller.jarvis.MyBundle
-import com.github.fmueller.jarvis.services.MyProjectService
-import com.github.fmueller.jarvis.services.OllamaService
 import javax.swing.JButton
 
-class MyToolWindowFactory : ToolWindowFactory {
-
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
+class ConversationWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val myToolWindow = MyToolWindow(toolWindow)
-        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
+        val conversationWindow = ConversationWindow(toolWindow)
+        val content = ContentFactory.getInstance().createContent(conversationWindow.getContent(), null, false)
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project) = true
 
-    class MyToolWindow(toolWindow: ToolWindow) {
+    class ConversationWindow(toolWindow: ToolWindow) {
 
         private val ollama = toolWindow.project.service<OllamaService>()
         private val service = toolWindow.project.service<MyProjectService>()
