155a5c324611de3d93ec48de12320635b087ba60
Felix Müller <felix@fmueller.io>
2024-05-14 12:37:26 +0200

    Add missing settings config for flexmark dependency

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,12 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("flexmark", "com.vladsch.flexmark:flexmark-all:0.64.8")
+        }
+    }
+}
+
 rootProject.name = "jarvis"
