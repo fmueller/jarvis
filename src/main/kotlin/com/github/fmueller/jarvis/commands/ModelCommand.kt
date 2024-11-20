19c9a7483985afbc2c9435b8a64c1a5723af03ca
Felix Müller <felix@fmueller.io>
2024-11-20 20:42:43 +0100

    refactor: Use setter for changing modelName

--- src/main/kotlin/com/github/fmueller/jarvis/commands/ModelCommand.kt
+++ src/main/kotlin/com/github/fmueller/jarvis/commands/ModelCommand.kt
@@ -8,7 +8,7 @@ class ModelCommand(private val modelName: String) : SlashCommand {
 
     override suspend fun run(conversation: Conversation): Conversation {
         if (modelName == "llama3.1" || modelName == "llama3.2") {
-            OllamaService.setModel(modelName)
+            OllamaService.modelName = modelName
             conversation.addMessage(Message.fromAssistant("Model changed to $modelName"))
         } else {
             conversation.addMessage(Message.fromAssistant("Invalid model name: $modelName"))
