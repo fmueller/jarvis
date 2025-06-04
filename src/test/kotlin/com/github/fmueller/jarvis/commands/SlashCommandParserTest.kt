3cfe52efee8b7f36420b45e1446be0e0ca4df258
Felix Müller <felix@fmueller.io>
2025-06-04 12:04:56 +0000

    Add SlashCommandParser tests

---
+++ src/test/kotlin/com/github/fmueller/jarvis/commands/SlashCommandParserTest.kt
@@ -0,0 +1,26 @@
+package com.github.fmueller.jarvis.commands
+
+import junit.framework.TestCase
+
+class SlashCommandParserTest : TestCase() {
+
+    fun `test parse help returns HelpCommand`() {
+        val command = SlashCommandParser.parse("/help")
+        assertTrue(command is HelpCommand)
+    }
+
+    fun `test parse new returns NewConversationCommand`() {
+        val command = SlashCommandParser.parse("/new")
+        assertTrue(command is NewConversationCommand)
+    }
+
+    fun `test parse plain returns PlainChatCommand`() {
+        val command = SlashCommandParser.parse("/plain hello")
+        assertTrue(command is PlainChatCommand)
+    }
+
+    fun `test parse model returns ModelCommand`() {
+        val command = SlashCommandParser.parse("/model llama3.1")
+        assertTrue(command is ModelCommand)
+    }
+}
