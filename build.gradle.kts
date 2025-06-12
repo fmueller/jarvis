4dc1a725496e6d1a2fe626a034a762faafb4a43b
Felix Müller <felix@fmueller.io>
2025-06-12 11:36:51 +0200

    build: change some build infrastructure versions and settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java") // Java support
     alias(libs.plugins.kotlin) // Kotlin support
-    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.intelliJPlatform) // IntelliJ Platform Gradle Plugin
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
