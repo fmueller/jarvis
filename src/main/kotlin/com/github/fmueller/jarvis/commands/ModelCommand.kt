0d60781d8cf17ab735ce4df1d17c68d71ba6b349
Felix Müller <felix@fmueller.io>
2025-06-12 19:37:25 +0200

    feat: change model update message

--- src/main/kotlin/com/github/fmueller/jarvis/commands/ModelCommand.kt
+++ src/main/kotlin/com/github/fmueller/jarvis/commands/ModelCommand.kt
@@ -7,8 +7,9 @@ import com.github.fmueller.jarvis.conversation.Message
 class ModelCommand(private val modelName: String) : SlashCommand {
 
     override suspend fun run(conversation: Conversation): Conversation {
-        OllamaService.modelName = if (modelName.equals("default", ignoreCase = true)) "qwen3:4b" else modelName
-        conversation.addMessage(Message.info("Model changed to $modelName"))
+        val newModel = if (modelName.equals("default", ignoreCase = true)) "qwen3:4b" else modelName
+        OllamaService.modelName = newModel
+        conversation.addMessage(Message.info("Model changed to $newModel"))
         return conversation
     }
 }
